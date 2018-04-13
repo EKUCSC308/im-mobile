c8a8f93b6564b30e75a9b1efa7807743bccbc50a
student <student@APC02HM0Z4DV7N-8.local>
2018-04-12 20:10:23 -0400

    Initial commit

---
+++ src/IM-Mobile-Proj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IM-Mobile-Proj
+//
+//  Created by student on 4/12/18.
+//  Copyright © 2018 cs@eku.edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
