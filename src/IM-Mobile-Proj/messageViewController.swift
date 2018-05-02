f8d6c9d68e35bbabf757adaf09555ac3db852616
student <student@APC02HM0Z4DV7N-10.local>
2018-05-02 19:05:49 -0400

    added sockets

--- src/IM-Mobile-Proj/messageViewController.swift
+++ src/IM-Mobile-Proj/messageViewController.swift
@@ -7,6 +7,15 @@
 //
 
 import UIKit
+import SocketIO
+
+struct ChatEvent: Decodable {
+    var content: String,
+    conversation_token: String,
+    device_token: String,
+    scope: String,
+    type: String
+}
 
 class messageViewController: UIViewController, UITextFieldDelegate {
     
@@ -18,6 +27,9 @@ class messageViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet weak var sendButton: UIButton!
+    
+    let manager = SocketManager(socketURL: URL(string: "http://192.241.175.100:3002")!, config: [.log(true),.connectParams(["token": "....", "jwt": "..."])])
+    var socket:SocketIOClient!
     
     override func viewDidLoad() {
         
@@ -49,7 +61,6 @@ class messageViewController: UIViewController, UITextFieldDelegate {
             
             mainScrollView.contentSize.height += CGFloat(50)
             mainScrollView.addSubview(label)
-            
         }
         
         mainScrollView.contentSize.height += CGFloat(50)
@@ -62,40 +73,74 @@ class messageViewController: UIViewController, UITextFieldDelegate {
         
         view.addSubview(mainScrollView)
         bottomScroll()
+        
+        self.socket = manager.defaultSocket
+        
+        // Execute when connection is established
+        self.socket.on(clientEvent: .connect) {data, ack in
+            print("SOCKET MANAGER: socket connected");
+        }
+        
+        self.socket.on("event") {data, ack in
+            if let arr = data as? [[String: Any]] {
+                print("test")
+                
+                let eventResponse = ChatEvent(
+                    content: arr[0]["content"]! as! String,
+                    conversation_token: arr[0]["conversation_token"]! as! String,
+                    device_token: arr[0]["device_token"]! as! String,
+                    scope: arr[0]["scope"]! as! String,
+                    type: arr[0]["type"]! as! String
+                )
+                
+                self.handleChatEvent(event: eventResponse)
+            }
+        }
+        
+        
+        // Executes when connection is inturrupted
+        self.socket.on(clientEvent: .disconnect) {data, ack in
+            print("SOCKET MANAGER: socket disconnected");
+        }
+        
+        self.socket.connect()
     }
     
     @IBAction func send(_ sender: Any) {
         view.endEditing(true)
         let newText = textField.text
-        if(newText != nil){
-            let label = UILabel()
-            label.text = newText
-            label.backgroundColor = .yellow
-            
-            print(50*messages.count)
-            
-            label.frame = CGRect(x: 0, y: CGFloat(50*(messages.count)), width: 150, height: 35)
-            
-            print(mainScrollView.contentSize.height)
-            
-            mainScrollView.contentSize.height = mainScrollView.contentSize.height + CGFloat(50)
-            
-            print(mainScrollView.contentSize.height)
-            
-            mainScrollView.addSubview(label)
-        
-            textField.frame = CGRect(x:0, y: CGFloat(mainScrollView.contentSize.height-50), width: 300, height: 35)
-        
-            sendButton.frame = CGRect(x:300, y:CGFloat(mainScrollView.contentSize.height-50), width: 50, height: 35)
-            
-            messages.append((newText)!)
+        if(newText != nil) {
+            renderMessage(message: newText!, isOwnMessage: true)
             textField.text = ""
+            
+            // TODO >>>>>>>>>>>>>> emit to server (something like self.socket.emit('event, ChatEvent(...))
         }
-        bottomScroll()
     }
     
-
-    
+    func renderMessage(message: String, isOwnMessage: Bool) {
+        let label = UILabel()
+        label.text = message
+        label.backgroundColor = .yellow
+        
+        print(50*messages.count)
+        
+        label.frame = CGRect(x: 0, y: CGFloat(50*(messages.count)), width: 150, height: 35)
+        
+        print(mainScrollView.contentSize.height)
+        
+        mainScrollView.contentSize.height = mainScrollView.contentSize.height + CGFloat(50)
+        
+        print(mainScrollView.contentSize.height)
+        
+        mainScrollView.addSubview(label)
+        
+        textField.frame = CGRect(x:0, y: CGFloat(mainScrollView.contentSize.height-50), width: 300, height: 35)
+        
+        sendButton.frame = CGRect(x:300, y:CGFloat(mainScrollView.contentSize.height-50), width: 50, height: 35)
+        
+        messages.append(message)
+        bottomScroll()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -105,6 +150,23 @@ class messageViewController: UIViewController, UITextFieldDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         registerKeyboardNotifications()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    func handleChatEvent(event: ChatEvent) {
+        if (event.scope == "session") {
+            // render non-message on UI
+        } else {
+            // render message
+            renderMessage(message: event.content, isOwnMessage: false)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.socket.disconnect()
     }
     
     func registerKeyboardNotifications() {
