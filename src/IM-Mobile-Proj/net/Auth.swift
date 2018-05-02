f6589a8b81203b59122581a0c42ad06ab2d83120
student <student@APC02HM0Z4DV7N-10.local>
2018-05-02 15:47:40 -0400

    added net code

--- src/IM-Mobile-Proj/net/Auth.swift
+++ src/IM-Mobile-Proj/net/Auth.swift
@@ -6,15 +6,47 @@
 //  Copyright © 2018 cs@eku.edu. All rights reserved.
 //
 import Foundation
-struct LoginResponse{
+struct LoginResponse:Decodable{
     var jwt: String
 }
 
-class Auth {
+struct RegisterResponse:Decodable {
+    var success: Bool
+}
+
+class AuthService {
     let httpRequest: HttpRequestHelper
     
     init () {
         self.httpRequest = HttpRequestHelper()
+    }
+    
+    func register(username: String, password: String, cb: @escaping (_ response: RegisterResponse?, _ error: String?) -> Void) throws {
+        // Convert the dictionary to JSON
+        let payload = try JSONSerialization.data(withJSONObject: [
+            "username": username,
+            "password": password
+            ])
+        
+        // Create the reqest
+        let request = httpRequest.createRequest(method: "POST", endpoint: "/auth/register", payload: payload)
+        
+        // Create a new thread to handle the request
+        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+            if (error != nil) {
+                cb(nil, "User already exists")
+            } else {
+                do {
+                    let registerResponse = try JSONDecoder().decode(RegisterResponse.self, from: data!)
+                    cb(registerResponse, nil)
+                } catch {
+                    cb(nil, "Failed to register")
+                }
+            }
+        }
+        
+        // Execute the request on its own thread
+        task.resume()
     }
     
     func login(username: String, password: String, cb: @escaping (_ response: LoginResponse?, _ error: String?) -> Void) throws {
@@ -22,7 +54,7 @@ class Auth {
         let payload = try JSONSerialization.data(withJSONObject: [
             "username": username,
             "password": password
-        ])
+            ])
         
         // Create the reqest
         let request = httpRequest.createRequest(method: "POST", endpoint: "/auth/login", payload: payload)
@@ -32,7 +64,12 @@ class Auth {
             if (error != nil) {
                 cb(nil, "Failed to login.")
             } else {
-                cb(LoginResponse(jwt: "..."), nil)
+                do {
+                    let loginResponse = try JSONDecoder().decode(LoginResponse.self, from: data!)
+                    cb(loginResponse, nil)
+                } catch {
+                    cb(nil, "Failed to login")
+                }
             }
         }
         
